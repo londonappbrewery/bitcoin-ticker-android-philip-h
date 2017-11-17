660f19dd192e6868af597bafa59146f6fefbe6e1
Philip Habib <philip.habib6347@gmail.com>
2017-11-17 01:00:02 -0500

    Project Done

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -8,20 +8,23 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
-    // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -44,35 +47,56 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                Log.d("Bitcoin", "Selected " + parent.getItemAtPosition(position));
+                String symbol = "BTC" + parent.getItemAtPosition(position).toString();
+                letsDoSomeNetworking(BASE_URL + symbol);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                Log.d("Bitcoin", "Nothing selected");
+            }
+        });
+    }
+    
+    private void letsDoSomeNetworking(String url)
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response)
+            {
+                super.onSuccess(statusCode, headers, response);
+                Log.d("Bitcoin", "Success! " + response.toString());
+                updateUI(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable)
+            {
+                super.onFailure(statusCode, headers, responseString, throwable);
+                Log.d("Bitcoin", "Fail! " + statusCode);
+                Toast.makeText(MainActivity.this, "Failed to get bitcoin value. Error: " + responseString, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
-
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
+    private void updateUI(JSONObject response)
+    {
+        try
+        {
+            String ask = response.getString("ask");
+            mPriceTextView.setText(ask);
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 
